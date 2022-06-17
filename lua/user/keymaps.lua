85c4589ada55a6bf26c485154eb3b756972cd544
Huy Dang <huy.dang.quoc@vieon.vn>
2022-06-17 15:50:48 +0700

    auto vim.lsp.buf.formatting_sync()

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -13,6 +13,8 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
 
 -- Bufferline
 keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", opts)
@@ -41,3 +43,6 @@ keymap("n", "<C-p>", ":Telescope find_files<CR>", opts)
 keymap("n", "<C-g>", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>th", ":lua require'telescope.builtin'.colorscheme(require('telescope.themes').get_dropdown{})<CR>", opts)
 
+--Gitsigns
+keymap("n", "<leader>b", ":Gitsigns blame_line<CR>", opts)
+keymap("n", "<leader>d", ":Gitsigns diffthis<CR>", opts)
